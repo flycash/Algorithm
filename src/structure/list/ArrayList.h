93f67cbce8bc3c7f7aadd260f31394758d676911
flycash <ming_flycash@foxmail.com>
2018-12-16 22:01:46 +0800

    :star2:define the interface of array list

--- src/structure/list/ArrayList.h
+++ src/structure/list/ArrayList.h
@@ -4,3 +4,18 @@ typedef struct ArrayList {
     int incrementFactor;
     void * data[];
 }
+
+void add(ArrayList list, void* data);
+
+void insert(ArrayList list, int index, void* data);
+
+void replace(ArrayList list, int index, void* data);
+
+void remove(ArrayList list, int index);
+
+void size(ArrayList list);
+
+bool isEmpty(ArrayList list);
+
+void clear(ArrayList list);
+
