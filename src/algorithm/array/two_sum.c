d55e703f9a7bc180c4d43719867da8619416585b
zavier.deng <zavier.deng@gllue.com>
2018-12-19 19:36:37 +0800

    add ignore

--- src/algorithm/array/two_sum.c
+++ src/algorithm/array/two_sum.c
@@ -1,9 +1,19 @@
-// leetcode 1 two sum 
-// https://leetcode.com/problems/two-sum
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
+/*
+ * =====================================================================================
+ *
+ *       Filename:  two_sum.c
+ *
+ *    Description:  leetcode two sum, see https://leetcode.com/problems/two-sum/
+ *
+ *        Version:  1.0
+ *        Created:  2018/12/19 10时58分46秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  flycash (https://www.jianshu.com/u/f2b47e5528d8), ming_flycash@foxmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
  */
-int* twoSum(int* nums, int numsSize, int target) {
-    
-}
+
+
