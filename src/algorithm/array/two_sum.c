85ec379a295cf0e0c49ae3cab27776dff9115a48
zavier.deng <zavier.deng@gllue.com>
2018-12-19 10:13:03 +0800

    :star2: init two_sum

---
+++ src/algorithm/array/two_sum.c
@@ -0,0 +1,9 @@
+// leetcode 1 two sum 
+// https://leetcode.com/problems/two-sum
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    
+}
