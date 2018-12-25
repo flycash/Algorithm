5c16f33ea68140fd1a0674aeb8128c4e35c94453
flycash <ming_flycash@foxmail.com>
2018-12-25 23:04:19 +0800

    :star2: sort implement

---
+++ src/algorithm/array/TwoSumSort.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  TwoSumSort.c
+ *
+ *    Description:  the solution by sorting the array,
+ * https://leetcode.com/problems/two-sum/.
+ * - sort the nums
+ * - find the number a and b that meet a+b = target
+ * - find the index of a and b
+ *
+ *        Version:  1.0
+ *        Created:  2018/12/23 00时59分54秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  flycash (https://www.jianshu.com/u/f2b47e5528d8),
+ * ming_flycash@foxmail.com Organization:
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int* result = malloc(sizeof(int) * 2);
+
+    return result;
+}
