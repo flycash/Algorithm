2f5fc9e0351b16b9898a42f1de1c1fbaadf0caad
flycash <mingflycash@gmail.com>
2019-01-23 23:28:04 +0800

    :tada: start the binary tree. red-black tree is too complicate to understand

---
+++ src/structure/tree/BinarySearchTree.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  BinarySearchTree.c
+ *
+ *    Description:  binary search tree
+ *
+ *        Version:  1.0
+ *        Created:  01/23/2019 11:23:33 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  flycash (https://github.com/flycash,
+ * https://www.jianshu.com/u/f2b47e5528d8), ming_flycash@foxmail.com
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode* leftChild;
+    struct TreeNode* rightChild;
+} TreeNode;
+
+void insertIntoBinaryTree(TreeNode* root, int data) {}
---
+++ src/structure/tree/BinarySearchTreeTest.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  BinarySearchTreeTest.c
+ *
+ *    Description:  test for BinarySearchTree.c
+ *
+ *        Version:  1.0
+ *        Created:  01/23/2019 11:25:21 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  flycash (https://github.com/flycash,
+ * https://www.jianshu.com/u/f2b47e5528d8), ming_flycash@foxmail.com
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#include "BinarySearchTree.c"
+
+int main() { return 0; }
+
