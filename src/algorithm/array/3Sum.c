fa9d0f9cf74c07a1e0641fe3defd5204d39a320c
flycash <mingflycash@gmail.com>
2019-02-22 17:07:20 +0800

    :sparkles:3sum

---
+++ src/algorithm/array/3Sum.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  3Sum.c
+ *
+ *    Description:  3Sum. https://leetcode.com/problems/3sum/
+ *
+ *        Version:  1.0
+ *        Created:  02/22/2019 04:39:46 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  flycash (https://github.com/flycash,
+ * https://www.jianshu.com/u/f2b47e5528d8), ming_flycash@foxmail.com
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int** threeSum(int* nums, int numsSize, int* returnSize) {}
+
